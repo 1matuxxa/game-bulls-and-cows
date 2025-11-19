fc4909c59a8e822f6327c6fd786a82718d79ff11
Baki <egcuzn3tsov@yandex.ru>
2025-11-19 09:54:27 +0500

    Add files via upload

---
+++ main-game/Bull and Cows(Логика готова).py
@@ -0,0 +1,154 @@
+import random
+import time
+
+def main_game_medium(start_time):
+    mechanical_digits = ""
+    while len(mechanical_digits) != 4:
+        random_digit = str(random.randint(0,9))
+        if random_digit not in mechanical_digits:
+            mechanical_digits += str(random_digit)
+    count = 0
+
+    while True:
+        flag = True
+        cows = 0
+        bulls =  0
+        user_guess = str(input('Введите ваше четырехзначное число:'))
+        if len(user_guess) != 4:
+            print('Число должно состоять из 4 элементов')
+            continue
+        for i in user_guess:
+            if user_guess.count(i) != 1:
+                flag = False
+                break
+        if flag == False:
+            if count == 0: count += 0
+            else: count -= 0
+            print(f'Число должно быть неповторяющимся, количество ваших попыток: {count}')
+            continue
+        if flag:
+            for i in range(4):
+                if user_guess[i] == (mechanical_digits)[i]:
+                    bulls += 1
+                if user_guess[i] in (mechanical_digits) and user_guess[i] != mechanical_digits[i]:
+                    cows += 1
+            count += 1
+            if count == 10:
+                return print('Вы исчерпали количество попыток, хотите попробовать еще раз?')
+            if bulls != 4:
+                print(f'Коровы: {cows}')
+                print(f'Быки: {bulls}')
+                print(f'Попыток: {count}')
+            else:
+                elapsed = time.time() - start_time
+                minutes = int(elapsed // 60)
+                seconds = int(elapsed % 60)
+                print("~" * 100)
+                print(f'Вы выйграли!!! Ваше количество попыток: {count}')
+                print(f'Ваше время: Минуты - {minutes}, Секунды - {seconds}')
+                print("~" * 100)
+
+def main_game_easy(start_time):
+    mechanical_digits = ""
+    while len(mechanical_digits) != 3:
+        random_digit = str(random.randint(0,9))
+        if random_digit not in mechanical_digits:
+            mechanical_digits += str(random_digit)
+    count = 0
+
+    while True:
+        flag = True
+        cows = 0
+        bulls =  0
+        user_guess = str(input('Введите ваше трехзначное число:'))
+        if len(user_guess) != 3:
+            print('Число должно состоять из 3 элементов')
+            continue
+        for i in user_guess:
+            if user_guess.count(i) != 1:
+                flag = False
+                break
+        if flag == False:
+            if count == 0: count += 0
+            else: count -= 0
+            print(f'Число должно быть неповторяющимся, количество ваших попыток: {count}')
+            continue
+        if flag:
+            for i in range(3):
+                if user_guess[i] == (mechanical_digits)[i]:
+                    bulls += 1
+                if user_guess[i] in (mechanical_digits) and user_guess[i] != mechanical_digits[i]:
+                    cows += 1
+            count += 1
+            if count == 15:
+                return print('Вы исчерпали количество попыток, хотите попробовать еще раз?')
+            if bulls != 3:
+                print(f'Коровы: {cows}')
+                print(f'Быки: {bulls}')
+                print(f'Попыток: {count}')
+            else:
+                elapsed = time.time() - start_time
+                minutes = int(elapsed // 60)
+                seconds = int(elapsed % 60)
+                print("~" * 100)
+                print(f'Вы выйграли!!! Ваше количество попыток: {count}')
+                print(f'Ваше время: {minutes} мин {seconds}.{int((elapsed % 1)*100):02d} сек')
+                print("~" * 100)
+
+def main_game_hard(start_time):
+    mechanical_digits = ""
+    while len(mechanical_digits) != 5:
+        random_digit = str(random.randint(0,9))
+        if random_digit not in mechanical_digits:
+            mechanical_digits += str(random_digit)
+    count = 0
+    while True:
+            m = []
+            flag = True
+            cows = 0
+            bulls =  0
+            user_guess = str(input('Введите ваше пятизначное число:'))
+            if len(user_guess) != 5:
+                print('Число должно состоять из 5 элементов')
+                continue
+            for i in user_guess:
+                if user_guess.count(i) != 1:
+                    flag = False
+                    break
+            if flag == False:
+                if count == 0: count += 0
+                else: count += 0
+                print(f'Число должно быть неповторяющимся, количество ваших попыток: {count}')
+                continue
+            if flag:
+                for i in range(5):
+                    if user_guess[i] == (mechanical_digits)[i]:
+                        bulls += 1
+                    if user_guess[i] in (mechanical_digits) and user_guess[i] != mechanical_digits[i]:
+                        cows += 1
+                count += 1
+                if count == 7:
+                    return print(f'Вы исчерпали количество попыток, хотите попробовать еще раз?, Число загаданное компьютером: {mechanical_digits}')
+                if bulls != 5:
+                    print(f'Коровы: {cows}')
+                    print(f'Быки: {bulls}')
+                    print(f'Попыток: {count}')
+                else:
+                    elapsed = time.time() - start_time
+                    minutes = int(elapsed // 60)
+                    seconds = int(elapsed % 60)
+                    print("~" * 100)
+                    print(f'Вы выйграли!!! Ваше количество попыток: {count}')
+                    print(f'Ваше время: Минуты - {minutes}, Секунды - {seconds}')
+                    print("~" * 100)
+def change_the_difficulty():
+    start_time = time.time()
+    user_changer = input('Введите уровень сложности:')
+    if user_changer == 'Лёгкий':
+        main_game_easy(start_time)
+    if user_changer == 'Средний':
+        main_game_medium(start_time)
+    if user_changer == 'Сложный':
+        main_game_hard(start_time)
+
+change_the_difficulty()
